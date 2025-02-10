32ba6eed2acbcc1078843bb4f348aeef3da4fa90
dmytrodatsenko <dmitriidatsenko@gmail.com>
2025-02-10 21:40:47 +0200

    python again

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
             Save.loadState();
         });
         
-
         //Initialize editor panel
         Panel.createEditorPanel();
 
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   plugins: [],
   server: {
     host: true
   },
-  // build: {
-  //   outDir: 'dist',
-  //   sourcemap: true,
-  //   minify: 'esbuild',
-  //   terserOptions: {
-  //     compress: {
-  //       drop_console: true
-  //     }
-  //   }
-  // }
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'esbuild',
+    terserOptions: {
+      compress: {
+        drop_console: true
+      }
+    },
+  }
 });
